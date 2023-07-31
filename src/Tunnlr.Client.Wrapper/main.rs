83074d4ff5cb95fd909035e10098a7926604868e
Gerwim Feiken <info@gerwim.nl>
2023-07-31 14:13:34 +0200

    Improve Wry wrapper

--- src/Tunnlr.Client.Wrapper/main.rs
+++ src/Tunnlr.Client.Wrapper/main.rs
@@ -1,20 +1,34 @@
-use std::{path::PathBuf, fs, process::{Command}};
+use std::{fs, path::PathBuf, process::Command};
 
 use include_dir::{include_dir, Dir};
 
 static ASSETS: Dir = include_dir!("$CARGO_MANIFEST_DIR/assets");
 static MAIN_APPLICATION: &str = "Tunnlr.Client.Web";
 
+#[tokio::main]
+async fn main() -> wry::Result<()> {
+    let mut tunnlr_process;
 
-fn main() -> wry::Result<()> {
-    let temp_dir: PathBuf = std::env::temp_dir().join("Tunnlr");
-    let _ = fs::create_dir(temp_dir.clone());
-    let _ = ASSETS.extract(temp_dir.clone());
+    if cfg!(not(target_os = "macos")) {
+        let temp_dir: PathBuf = std::env::temp_dir().join("Tunnlr");
+        let _ = fs::create_dir(temp_dir.clone());
+        let _ = ASSETS.extract(temp_dir.clone());
 
-    // Start main application
-    let mut command = Command::new(temp_dir.clone().join(MAIN_APPLICATION));
-    command.current_dir(temp_dir.clone());
-    let mut process = command.spawn().unwrap();
+        // Start main application
+        let mut command = Command::new(temp_dir.clone().join(MAIN_APPLICATION));
+        command.current_dir(temp_dir.clone());
+        tunnlr_process = command.spawn().unwrap();
+    } else {
+        // Start main application
+        let exec = std::env::current_exe()?;
+        let executable_dir = exec.parent().expect("Should be a directory");
+
+        let mut command = Command::new(executable_dir.clone().join(MAIN_APPLICATION));
+        command.current_dir(executable_dir.clone());
+        tunnlr_process = command.spawn().unwrap();
+    }
+
+    wait_for_tunnlr_start().await;
 
     use wry::{
         application::{
@@ -37,7 +51,7 @@ fn main() -> wry::Result<()> {
         .build(&event_loop)?;
 
     let _webview = WebViewBuilder::new(window)?
-        .with_url("http://127.0.0.1:5109")?
+        .with_url("http://localhost:5109")?
         .with_new_window_req_handler(move |uri: String| {
             let submitted = proxy.send_event(UserEvent::NewWindow(uri.clone())).is_ok();
 
@@ -61,9 +75,24 @@ fn main() -> wry::Result<()> {
             } => {
                 *control_flow = ControlFlow::Exit;
 
-                let _ = process.kill();
-            },
+                let _ = tunnlr_process.kill();
+            }
             _ => (),
         }
     });
+}
+
+async fn wait_for_tunnlr_start() {
+    let http_client = reqwest::Client::new();
+    let mut is_ready = false;
+    while !is_ready {
+        let response = http_client.head("http://localhost:5109").send().await;
+
+        if response.is_ok() && response.unwrap().status() == reqwest::StatusCode::OK {
+            is_ready = true;
+        }
+
+        // Wait another round
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 }
