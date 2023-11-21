00e295bdf0ed172ddf562f4f525e1e9d81a65316
Gerwim Feiken <info@gerwim.nl>
2023-11-21 09:38:08 +0100

    chore(e2e): add sign in test

---
+++ test/e2e/tests/signin.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('?').click();
+  await page.getByText('Login').click();
+  await page.getByLabel('Email address').fill(process.env.AUTH_USERNAME);
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill(process.env.AUTH_PASSWORD);
+  await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await page.getByText('t', { exact: true }).click();
+  await page.getByText('Logout').click();
+});
